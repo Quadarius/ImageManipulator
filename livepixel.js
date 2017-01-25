beceda5ea029c2c00822b776b2b0a7984525a9c3
Quadarius Moore <qkmoore@crimson.ua.edu>
2017-01-25 00:41:37 -0600

    a bit of refactoring

--- livepixel.js
+++ livepixel.js
@@ -1,274 +1,226 @@
-var livePixel = function (){
+var Pixel = function(x, y, maxSpeed, maxSize) {
+	this.x = x;
+	this.y = y;
+	this.speed = Math.max(Math.random() * maxSpeed, 1);
+	this.size = Math.max(Math.random() * maxSize, 5)
+}
+
+Pixel.prototype.reset = function(x, y, maxSpeed, maxSize) {
+	Pixel.apply(this, arguments);
+	return this
+}
+
+var PixelFactory = {
+
+	getRandomPixel: function(w, h, maxSpeed, maxSize) {
+		var coords = PixelFactory.getRandomPosition(w, h);
+		return new Pixel(coords.x, coords.y, maxSpeed, maxSize)
+	},
+
+	getRandomPosition: function(w, h) {
+		return {
+			x: Math.floor(Math.random() * w),
+			y: Math.floor(Math.random() * h)
+		};
+	}
+}
+
+var PixelDisplay = function(numPixels, maxPixelSpeed, maxPixelSize) {
 	var self = this;
 
-	this.mode = '0'
-	this.pixelshape = 'hline';
-	this.travelstyle = 'down';
-	this.trail = true;
-	this.background = true;
-	this.noiseScale = 200;
-	this.noiseStrength = 32;
-	this.reverse = false;
+	this.pixelShape = 'square';
+	this.travelStyle = 'down';
+	this.numPixels = numPixels || 100;
+	this.pixelMaxSpeed = maxPixelSpeed || 5;
+	this.pixelMaxSize = maxPixelSize || 10;
 
-	this.numPixels = 300
-	this.threshold = 128;
-	this.init = function(){
-		_init();
-	}
-	this.chooseFile = function(){
-		document.getElementById('filepicker').click();
-	}
-	this.source = 'heic0307g.jpg';
-	this.maxSize = 10;
-	this.speed = 3;
-	this.growthRate = 0;
-	this.clear = function(){
-		og.clearRect(0, 0, width, height);
-	}
+	this.canvasElem = document.createElement('canvas');
+	this.bufferCanvasElem = document.createElement('canvas');
+	// this.backgroundCanvasElem = document.createElement('canvas');
+	this.canvas = this.canvasElem.getContext('2d');
+	this.buffer = this.bufferCanvasElem.getContext('2d');
 
-	var img = new Image(),
-		ogImg = new Image(),
-		height, width,
-		source;
+	this.width;
+	this.height;
 
-	var canvas = document.createElement('canvas'),
-		og = canvas.getContext('2d');
+	this.LivePixels = [];
 
-	var sdCanvas = document.createElement('canvas'),
-		sd = sdCanvas.getContext('2d');
+	this.imageSample = new Image();
+	this.pixels = [];
+	this.imageData;
+	this.counter = 0;
 
-	var pixels = [], imageData;
-	var bitmap = [], bImageData;
+	this.imageSample.onload = function(){
+		self.height = this.height;
+		self.width = this.width;
+		console.log(PixelDisplay)
 
-	var livePixels = [];
-	var counter = 0;
-
-	function _init(){
-		setup();
-		// return canvas;
-		document.getElementById('canvas').appendChild(canvas);
+		self.canvasElem.height = self.bufferCanvasElem.height = this.height;
+		self.canvasElem.width = self.bufferCanvasElem.width = this.width;
+		self.render();
 	}
 
-	function setup(){
-		livePixels = [];
-		img.onload = function(){
-			height = img.height;
-			width = img.width;
-
-			canvas.height = sdCanvas.height = height;
-			canvas.width = sdCanvas.width = width;
-
-			draw(this, self.mode);
-		}
-
-		img.src = ogImg.src = self.source || 'heic0307g.jpg';
-	}
-
-	function draw(img, mode){
-		if(self.background){
-			og.drawImage(ogImg, 0, 0);
-		} else {
-			og.rect(0, 0, width, height);
-			og.fill()
-		}
-		sd.drawImage(ogImg, 0, 0);
-		pixels = sd.getImageData(0, 0, width, height).data;
-		bImageData = og.getImageData(0, 0, width, height);
-		bImageData = threshold(bImageData);
-		og.putImageData(bImageData, 0, 0);
-
-		switch (mode){
-			case '0':
-				livePixel(img);
-				break;
-
-			default:
-		}
-	}
-
-
-	function generatePixels(){
-		for(var i = 0; i < self.numPixels; i++){
-			livePixels.push(new Pixel(Math.random() * width, Math.random() * height));
-		}
-	}
-
-	function threshold(px){
-		 var d = px.data;
-		for (var i=0; i<d.length; i+=4) {
-			var r = d[i];
-			var g = d[i+1];
-			var b = d[i+2];
-			var v = (0.2126*r + 0.7152*g + 0.0722*b >= self.threshold) ? 255 : 0;
-			d[i] = d[i+1] = d[i+2] = v
-		}
-
-		return px;
-	}
-
-	function livePixel(im){
-		generatePixels();
-		createBitmap(im);
-		loop();
-	}
-
-	function createBitmap(img) {
-		var imageData = og.getImageData(0, 0, width, height);
-		bitmap = threshold(imageData, 128).data;
-	};
-
-	function render() {
-		if(!self.trail) {
-			og.clearRect(0, 0, width, height);
-			//og.drawImage(ogImg, 0, 0);
-		};
-		og.globalCompositeOperation = "darker";
-
-		for (var i = 0; i < livePixels.length; i++) {
-			livePixels[i].render();
-		}
-	};
-
-	function loop(){
-		requestAnimationFrame(loop);
-		render();
-	}
-
-	function getColor(x, y) {
-		var base = (Math.floor(x) + Math.floor(y) * width) * 4;
-		var c = {
-			r: bitmap[base + 0],
-			g: bitmap[base + 1],
-			b: bitmap[base + 2],
-			a: bitmap[base + 3]
-		};
-
-		return "rgb(" + c.r + "," + c.g + "," + c.b + ")";
-	};
-
-	function getOgColor(x, y) {
-		var base = (Math.floor(x) + Math.floor(y) * width) * 4;
-		var c = {
-			r: pixels[base + 0],
-			g: pixels[base + 1],
-			b: pixels[base + 2],
-			a: pixels[base + 3]
-		};
-
-		return "rgb(" + c.r + "," + c.g + "," + c.b + ")";
-	};
-
-	function Pixel(x, y, c) {
-		this.x = x;
-		this.y = y;
-
-		this.r = Math.random() * self.maxSize;
-		this.angle = Math.random() * 360;
-		this.grow = (this.r > self.maxSize/2 ? true : false);
-
-		this.color = getOgColor(this.x, this.y);
-
-		this.render = function () {
-			var c = getColor(this.x, this.y);
-
-			var onScreen = this.x >= 0 && this.x <= width &&
-				this.y >= 0 && this.y <= height;
-
-			var isBlack = c != "rgb(255,255,255)" && onScreen;
-
-			if (onScreen) {
-				// update the pixel size
-				if(this.r >= self.maxSize){
-					this.grow = false;
-				}else if(this.r <= 0){
-					this.grow = true;
-				}
-				if(this.grow){
-					this.r += self.growthRate;
-				}else{
-					this.r -= self.growthRate;
-				}
-
-				// update the pixels position
-				switch(self.travelstyle){
-					case 'up':
-						this.y -= self.speed;
-						break;
-					case 'down':
-						this.y += self.speed;
-						break;
-					case 'left':
-						this.x -= Math.random() * self.speed;
-						break;
-					case 'right':
-						this.x += Math.random() * self.speed;
-						break;
-					case 'random':
-						this.x = Math.random() * width;
-						this.y = Math.random() * height;
-						break;
-				}
-
-				// draw the pixel shape
-				switch(self.pixelshape){
-					case 'circle':
-						og.fillStyle = getOgColor(this.x, this.y);
-						og.beginPath();
-						og.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-						og.fill();
-						break;
-					case 'square':
-						og.fillStyle = getOgColor(this.x, this.y);
-						og.beginPath();
-						og.rect(this.x, this.y, this.r, this.r);
-						og.fill();
-						break;
-					case 'spinline':
-						var tx = this.x, ty = this.y;
-						og.strokeStyle = getOgColor(this.x, this.y);
-						og.beginPath();
-						og.moveTo(this.x, this.y);
-						tx += Math.cos(this.angle % 360) * self.speed;
-						ty += -Math.sin(this.angle % 360) * self.speed;
-						og.lineTo(tx, ty);
-						og.lineWidth = this.r;
-						og.stroke();
-						this.angle += self.growthRate;
-						break;
-					case 'hline':
-						var tx = this.x, ty = this.y;
-						og.strokeStyle = getOgColor(this.x, this.y);
-						og.beginPath();
-						og.moveTo(this.x, this.y);
-						tx += this.r;
-						og.lineTo(tx, ty);
-						og.lineWidth = self.speed;
-						og.stroke();
-						break;
-					case 'vline':
-						var tx = this.x, ty = this.y;
-						og.strokeStyle = getOgColor(this.x, this.y);
-						og.beginPath();
-						og.moveTo(this.x, this.y);
-						ty += this.r;
-						og.lineTo(tx, ty);
-						og.lineWidth = self.speed;
-						og.stroke();
-						break;
-				}
-			}else {
-				if(this.x > width){
-					this.x = 0;
-				}else if(this.x < 0){
-					this.x = width;
-				}
-				if(this.y > height){
-					this.y = 0;
-				}else if(this.y < 0){
-					this.y = height;
-				}
+	this.travelStyles = {
+		'up': function(pixel) {
+			pixel.y -= pixel.speed
+		},
+		'down': function(pixel) {
+			pixel.y += pixel.speed;
+		},
+		'left': function (pixel) {
+			pixel.x -= pixel.speed;
+		},
+		'right': function (pixel) {
+			pixel.x += pixel.speed;
+		},
+		'random': function (pixel) {
+			var direction = Math.ceil(Math.random() * 4);
+			switch(direction){
+				case 1:
+					pixel.y -= pixel.speed;
+					// pixel.y -= Math.random() * self.speed;
+					break;
+				case 2:
+					pixel.y += pixel.speed;
+					// pixel.y += Math.random() * self.speed;
+					break;
+				case 3:
+					pixel.x -= pixel.speed;
+					// pixel.x -= Math.random() * self.speed;
+					break;
+				case 4:
+					pixel.x += pixel.speed;
+					// pixel.x += Math.random() * self.speed;
+					break;
 			}
 		}
 	}
 
+	this.pixelShapes = {
+		'circle': function(pixel){
+			self.buffer.beginPath();
+			self.buffer.fillStyle = self.getColor(pixel.x, pixel.y);
+			self.buffer.arc(pixel.x, pixel.y, pixel.size, 0, Math.PI * 2, false);
+			self.buffer.fill();
+		},
+		'square': function(pixel){
+			self.buffer.fillStyle = self.getColor(pixel.x, pixel.y);
+			self.buffer.fillRect(pixel.x, pixel.y, pixel.size, pixel.size);
+		},
+		// 'spinline': function (pixel) {
+		// 	var tx = this.x, ty = this.y;
+		// 	buffer.strokeStyle = getOgColor(this.x, this.y);
+		// 	buffer.beginPath();
+		// 	buffer.moveTo(this.x, this.y);
+		// 	tx += Math.cos(mine.angle % 360) * pixel.speed;
+		// 	ty += -Math.sin(mine.angle % 360) * pixel.speed;
+		// 	buffer.lineTo(tx, ty);
+		// 	buffer.lineWidth = this.r;
+		// 	buffer.closePath();
+		// },
+		'hline': function (pixel) {
+			var tx = pixel.x, ty = pixel.y;
+			self.buffer.strokeStyle = self.getColor(pixel.x, pixel.y);
+			self.buffer.beginPath();
+			self.buffer.moveTo(pixel.x, pixel.y);
+			tx += pixel.size;
+			self.buffer.lineTo(tx, ty);
+			self.buffer.lineWidth = pixel.speed;
+			self.buffer.closePath();
+			self.buffer.stroke();
+		},
+		'vline': function (pixel) {
+			var tx = pixel.x, ty = pixel.y;
+			self.buffer.strokeStyle = self.getColor(pixel.x, pixel.y);
+			self.buffer.beginPath();
+			self.buffer.moveTo(pixel.x, pixel.y);
+			ty += pixel.size;
+			self.buffer.lineTo(tx, ty);
+			self.buffer.lineWidth = pixel.speed;
+			self.buffer.stroke();
+		},
+		'zoom': function (pixel) {
+			// self.buffer.drawImage(self.canvasElem,
+						self.buffer.drawImage(self.imageSample,
+				pixel.x,
+				pixel.y,
+				Math.max(pixel.size / 2, 5), Math.max(pixel.size / 2, 5),
+				pixel.x, pixel.y,
+				Math.max(pixel.size, 10), Math.max(pixel.size, 10));
+		}
+	}
+}
+
+PixelDisplay.prototype.getTravelStyle = function (style) {
+	return self.travelStyles[style]
+}
+
+PixelDisplay.prototype.updatePixels = function () {
+	var i,
+		pixel,
+		newPosition;
+
+	for(i =0; i < this.numPixels; i++){
+		pixel = this.LivePixels[i];
+		if(this.isOnScreen(pixel)){
+			this.travelStyles[this.travelStyle](pixel);
+			this.pixelShapes[this.pixelShape](pixel);
+		}else{
+			newPosition = PixelFactory.getRandomPosition(this.width, this.height);
+			pixel.reset(newPosition.x, newPosition.y, this.pixelMaxSpeed, this.pixelMaxSize);
+		}// end if else
+	}//end For loop
+	this.canvas.drawImage(this.bufferCanvasElem, 0, 0);
+}
+
+PixelDisplay.prototype.getColor = function(x, y) {
+	var base = (Math.floor(x) + Math.floor(y) * this.width) * 4;
+	var c = {
+		r: this.pixels[base + 0],
+		g: this.pixels[base + 1],
+		b: this.pixels[base + 2],
+		a: this.pixels[base + 3]
+	};
+
+	return "rgb(" + c.r + "," + c.g + "," + c.b + ")";
+}
+
+PixelDisplay.prototype.isOnScreen = function(pixel){
+	return (pixel.x >= 0 - this.pixelMaxSize) &&
+			(pixel.y >= 0 - this.pixelMaxSize) &&
+			(pixel.x <= this.width + this.pixelMaxSize) &&
+			(pixel.y <= this.height + this.pixelMaxSize);
+}
+
+PixelDisplay.prototype.chooseFile = function(){
+	document.getElementById('filepicker').click();
+}
+
+
+PixelDisplay.prototype.loop = function () {
+	requestAnimationFrame(this.loop.bind(this));
+	this.updatePixels();
+}
+
+PixelDisplay.prototype.render = function(){
+	this.buffer.imageSmoothingEnabled = false;
+	this.buffer.mozImageSmoothingEnabled = false;
+	this.buffer.webkitImageSmoothingEnabled = false;
+	this.buffer.msImageSmoothingEnabled = false;
+
+	this.canvas.drawImage(this.imageSample, 0, 0);
+	this.imageData = this.canvas.getImageData(0, 0, this.width, this.height);
+	this.pixels = this.imageData.data;
+	this.buffer.drawImage(this.imageSample, 0, 0);
+
+	for(var i = 0; i < this.numPixels; i++){
+		this.LivePixels.push(PixelFactory.getRandomPixel(this.width, this.height, this.pixelMaxSpeed, this.pixelMaxSize));
+	}
+
+	this.loop();
+
+	document.getElementById('canvas').appendChild(this.canvasElem);
+}
+
+PixelDisplay.prototype._init = function () {
 }
