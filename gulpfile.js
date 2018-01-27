99dffd7e7972c8df651a81a6c285e5a1c322491f
Quadarius Moore <qkmoore@crimson.ua.edu>
2018-01-26 23:12:02 -0600

    Add npm

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var server = require('browser-sync').create();
+
+var paths = {
+	styles: {
+		src: './**/*.css'
+	},
+	scripts: {
+		src: './**/*.js'
+	},
+	html: './**/*.html'
+};
+
+function reload(done) {
+	server.reload();
+	done();
+}
+
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	done();
+}
+
+
+function watch() {
+	gulp.watch( paths.html, reload );
+	gulp.watch( paths.styles.src, reload );
+	gulp.watch( paths.scripts.src, reload );
+}
+
+gulp.task( 'default',
+	gulp.series( serve, watch )
+);
